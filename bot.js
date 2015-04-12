62514acac85a3ffc7d4e435cbcf515c09ecc6f48
precla <aprcel00@fesb.hr>
2015-04-12 17:54:17 +0200

    Added Weather function

--- bot.js
+++ bot.js
@@ -101,9 +101,45 @@ bot.addListener('message', function(nick, to, text, message) {
 		}
 	} else if (args[0] == '!weather') {
 		if (!args[1]) {
-			bot.say(to, 'Missing arguments. Usage example: !weather Moscow');
+			bot.say(to, 'Missing arguments. Usage example: !weather Moscow; !weather zip 10000,hr');
 		} else {
-			var apiKey = '';
+			var currentWeather = 'http://api.openweathermap.org/data/2.5/weather?';
+			var metric = '&units=metric';
+			var apiKey = '&APPID=API_KEY';
+			var userInput;
+			args.shift();
+
+			// check if user wants to search by ZIP code or by city name
+			if (args[0] == 'zip') {
+				var userInput = 'zip=' + args[1];
+			} else {
+				args = args.join('');
+				var userInput = 'q=' + args;
+			}
+			userInput += metric + apiKey;
+			var openweatherLink = currentWeather + userInput;
+
+			request(openweatherLink, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var openweatherJson = JSON.parse(body);
+					if (openweatherJson.cod == 404) {
+						console.log('error while trying to get weather, "cod" code: ', openweatherJson.cod);
+						bot.say(to, 'Not found.');
+					} else if (openweatherJson.cod == '200') {
+						var sunrise = new Date(openweatherJson.sys.sunrise * 1000);
+						var sunset = new Date(openweatherJson.sys.sunset * 1000);
+
+						var openweatherSummary = 'The current temperature in ' + openweatherJson.name +
+												', ' + openweatherJson.sys.country + ' is: ' + openweatherJson.main.temp.toFixed(1) +
+												' C, ' + openweatherJson.weather[0].description + '. Pressure: ' + openweatherJson.main.pressure +
+												' hpa. Wind speed: ' + openweatherJson.wind.speed + ' m/s (' + (openweatherJson.wind.speed * 3.6).toFixed(2) + ' km/h).';
+
+						bot.say(to, openweatherSummary);
+					} else {
+						console.log('error while trying to get weather for: ', openweatherLink);
+					}
+				}
+			});
 		}
 	}
 
