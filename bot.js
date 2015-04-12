fdb4bc0d7d25e39d8178cd642910cf19eb7d625b
precla <aprcel00@fesb.hr>
2015-04-12 13:34:24 +0200

    Fixed code style based on grunt warnings

--- bot.js
+++ bot.js
@@ -39,73 +39,74 @@ bot.addListener('message', function(nick, to, text, message) {
 	var args = text.split(' ');
 
 	if (args[0] == '!wp') {
-        if (!args[1]) {
-            bot.say(to, 'Missing arguments. Usage example: !wp NodeJS');
-        } else {
-            args.shift();                                           //removes first arg and moves others by -1
-            var args = args.join(' ');                              //adds ' ' between every elemnt of array and saves as string
-            var titleSecondTry = args;                              //save a copy for later use - if string has to be capitalized: martin scorsese -> Martin Scorsese
-            var title = querystring.stringify({ titles: args });    //add titles= before string
-            var wiki = 'https://en.wikipedia.org/w/api.php?continue=&action=query&' + title + '&indexpageids=&prop=extracts&exintro=&explaintext=&format=json';
+		if (!args[1]) {
+			bot.say(to, 'Missing arguments. Usage example: !wp NodeJS');
+		} else {
+			args.shift();                                           // removes first arg and moves others by -1
+			var args = args.join(' ');                              // adds ' ' between every elemnt of array and saves as string
+			var titleSecondTry = args;                              // save a copy for later use - if string has to be capitalized: martin scorsese -> Martin Scorsese
+			var title = querystring.stringify({ titles: args });    // add titles= before string
+			var wiki = 'https://en.wikipedia.org/w/api.php?continue=&action=query&' + title + '&indexpageids=&prop=extracts&exintro=&explaintext=&format=json';
 
-            request(wiki, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var wikiSummary = JSON.parse(body);
-                    var pageId = wikiSummary.query.pageids[0];      //get pageID
-                    
-                    //if pageId is -1 than the article does not exist
-                    if (pageId == -1) {
-                        
-                        //Try again with changing first letter of every word to upper case
-                        titleSecondTry = titleSecondTry.replace(/[^\s]+/g, function (word) {
-                            return word.replace(/^./, function (first) {
-                                return first.toUpperCase();
-                            });
-                        });
+			request(wiki, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var wikiSummary = JSON.parse(body);
+					var pageId = wikiSummary.query.pageids[0];      // get pageID
 
-                        titleSecondTry = querystring.stringify({ titles: titleSecondTry });
+					// if pageId is -1 than the article does not exist
+					if (pageId == -1) {
 
-                        wiki = 'https://en.wikipedia.org/w/api.php?continue=&action=query&' + titleSecondTry + '&indexpageids=&prop=extracts&exintro=&explaintext=&format=json';
-                        request(wiki, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                wikiSummary = JSON.parse(body);
-                                pageId = wikiSummary.query.pageids[0];
-                                if (pageId == -1) {
-                                    bot.say(to, 'Article does not exist or could not be found. Sorry :C');
-                                } else {
-                                    wikiSummary = wikiSummary.query.pages[pageId].extract;
-                                    wikiSummary = wikiSummary.slice(0, 280);
-                                    checkRedirect = wikiSummary.slice(0, 70);
-                                    if (checkRedirect == 'This is a redirect from a title with another method of capitalisation.') {
-                                        bot.say(to, 'Article is redirecting, please use the following link: https://en.wikipedia.org/wiki/' + titleSecondTry.slice(7));
-                                    } else {
-                                        wikiSummary = wikiSummary.concat('... Read more: ' + 'https://en.wikipedia.org/wiki/' + titleSecondTry.slice(7));
-                                        bot.say(to, wikiSummary);
-                                    }
-                                }
-                            }
-                        });
-                    } else {
-                        wikiSummary = wikiSummary.query.pages[pageId].extract;
-                        wikiSummary = wikiSummary.slice(0, 280);
-                        var checkRedirect = wikiSummary.slice(0, 70);
-                        if (checkRedirect == 'This is a redirect from a title with another method of capitalisation.') {
-                            bot.say(to, 'Article is redirecting, please use the following link: https://en.wikipedia.org/wiki/' + title.slice(7));
-                        } else {
-                            wikiSummary = wikiSummary.concat('... Read more: ' + 'https://en.wikipedia.org/wiki/' + title.slice(7));
-                            bot.say(to, wikiSummary);
-                        }
-                    }
-                }
-            });
-        }
-    } else if (args[0] == '!weather') {
-        if (!args[1]) {
-            bot.say(to, 'Missing arguments. Usage example: !weather Moscow');
-        } else {
-            var apiKey = '';
-        }
-    }
+						// Try again with changing first letter of every word to upper case
+						titleSecondTry = titleSecondTry.replace(/[^\s]+/g, function (word) {
+							return word.replace(/^./, function (first) {
+								return first.toUpperCase();
+							});
+						});
+
+						titleSecondTry = querystring.stringify({ titles: titleSecondTry });
+
+						wiki = 'https://en.wikipedia.org/w/api.php?continue=&action=query&' + titleSecondTry + '&indexpageids=&prop=extracts&exintro=&explaintext=&format=json';
+						request(wiki, function (error, response, body) {
+							if (!error && response.statusCode == 200) {
+								wikiSummary = JSON.parse(body);
+								pageId = wikiSummary.query.pageids[0];
+								if (pageId == -1) {
+									bot.say(to, 'Article does not exist or could not be found. Sorry :C');
+								} else {
+									wikiSummary = wikiSummary.query.pages[pageId].extract;
+									wikiSummary = wikiSummary.slice(0, 280);
+									checkRedirect = wikiSummary.slice(0, 70);
+									if (checkRedirect == 'This is a redirect from a title with another method of capitalisation.') {
+										bot.say(to, 'Article is redirecting, please use the following link: https://en.wikipedia.org/wiki/' + titleSecondTry.slice(7));
+									} else {
+										wikiSummary = wikiSummary.concat('... Read more: ' + 'https://en.wikipedia.org/wiki/' + titleSecondTry.slice(7));
+										bot.say(to, wikiSummary);
+									}
+								}
+							}
+						});
+					} else {
+						wikiSummary = wikiSummary.query.pages[pageId].extract;
+						wikiSummary = wikiSummary.slice(0, 280);
+						var checkRedirect = wikiSummary.slice(0, 70);
+						if (checkRedirect == 'This is a redirect from a title with another method of capitalisation.') {
+							bot.say(to, 'Article is redirecting, please use the following link: https://en.wikipedia.org/wiki/' + title.slice(7));
+						} else {
+							wikiSummary = wikiSummary.concat('... Read more: ' + 'https://en.wikipedia.org/wiki/' + title.slice(7));
+							bot.say(to, wikiSummary);
+						}
+					}
+				}
+			});
+		}
+	} else if (args[0] == '!weather') {
+		if (!args[1]) {
+			bot.say(to, 'Missing arguments. Usage example: !weather Moscow');
+		} else {
+			var apiKey = '';
+		}
+	}
+
 });
 
 bot.addListener('error', function(message) {
