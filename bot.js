f639bf1cbfbe366af9a8a404584af9aa360295d9
precla <aprcel00@fesb.hr>
2015-04-14 17:22:57 +0200

    Added TvRage support with !tv, !last & !next

--- bot.js
+++ bot.js
@@ -22,7 +22,8 @@
 
 var irc = require('irc'),
 	request = require('request'),
-	querystring = require('querystring');
+	querystring = require('querystring'),
+	moment = require('moment');
 
 	// bot config
 var bot = new irc.Client('SERVER', 'BOTNAME', {
@@ -137,6 +138,73 @@ bot.addListener('message', function(nick, to, text, message) {
 						bot.say(to, openweatherSummary);
 					} else {
 						console.log('error while trying to get weather for: ', openweatherLink);
+					}
+				}
+			});
+		}
+	} else if (args[0] == '!tv' || args[0] == '!next' || args[0] == '!last') {
+		if (!args[1]) {
+			bot.say(to, 'Missing arguments. Usage example: !tv Suits; !next Top Gear; !last The Simpsons');
+		} else {
+			var argInput = args[0];
+			args.shift();
+			var args = args.join(' ');
+			var userInput = querystring.stringify({ show: args });
+			var tvrageLink = 'http://services.tvrage.com/tools/quickinfo.php?' + userInput;
+
+			request(tvrageLink, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var tvrageContent = body;
+					var checkIfExists = tvrageContent.slice(0, 7);
+					if (checkIfExists == 'No Show') {
+						bot.say(to, 'Not found. Try harder!');
+					} else {
+						var tvTitle = tvrageContent.slice(tvrageContent.indexOf('Name@') + 5, tvrageContent.indexOf('Show URL@') - 1);
+						var tvURL = tvrageContent.slice(tvrageContent.indexOf('URL@') + 4, tvrageContent.indexOf('Premiered@') - 1);
+						var nextEp;
+						var lastEp;
+
+						// if show ended or cancelled, than there is no upcoming episode
+						if (tvrageContent.indexOf('Status@Ended') >= '0' || tvrageContent.indexOf('Status@Ended') >= '0') {
+							nextEp = tvrageContent.slice(tvrageContent.indexOf('Status@') + 7, tvrageContent.indexOf('Classification@') - 1) + '. No more episodes';
+						} else {
+							nextEp = tvrageContent.slice(tvrageContent.indexOf('Next Episode@') + 13, tvrageContent.indexOf('RFC3339@') - 1);
+
+							// get remaining time until next episode
+							var timeOfNextEp = new Date(tvrageContent.slice(tvrageContent.indexOf('NODST@') + 6, tvrageContent.indexOf('Country@') - 1) * 1000);
+							var duration = moment.duration((timeOfNextEp - moment()), 'milliseconds');
+							duration = moment.duration(duration.asSeconds() - 1, 'seconds');
+
+							var timeUntilNext = duration.days() + ' days ' + duration.hours() + ' hours ' + duration.minutes() + ' mins ' + duration.seconds() + ' secs';
+
+							nextEp = 'Next Episode: in ' + timeUntilNext + ' | Number: S' + nextEp.slice(0, 2) + nextEp.slice(2, 5).replace('x', 'E') +
+									' | Title: ' + nextEp.slice(nextEp.indexOf('^') + 1, nextEp.lastIndexOf('^'));
+						}
+
+						// get info for latest episode
+						lastEp = tvrageContent.slice(tvrageContent.indexOf('Latest Episode@') + 15, tvrageContent.lastIndexOf('Next Episode@'));
+
+						// get passed time from last episode
+						var timeOfLastEp = moment(lastEp.slice(-12, -1, 'MMM-DD-YYYY'));				// time from tvrage, last episode
+						var currentTime = moment();														// current time
+						var duration = moment.duration(currentTime - timeOfLastEp, 'milliseconds');
+
+						var timeFromLast = currentTime.diff(timeOfLastEp, 'days') + ' days ' + duration.hours() + ' hours ' + duration.minutes() + ' mins ' + duration.seconds() + ' secs';
+
+						lastEp = 'Latest Episode was ' + timeFromLast + ' ago | Number: S' + lastEp.slice(0, 2) + lastEp.slice(2, 5).replace('x', 'E') +
+									' | Title: ' + lastEp.slice(lastEp.indexOf('^') + 1, lastEp.lastIndexOf('^'));
+
+						var tvrageContentSummary = tvTitle + ' | ';
+						if (argInput == '!tv') {
+							tvrageContentSummary = tvrageContentSummary + lastEp + ' | ' + tvURL;
+						} else if (argInput == '!next') {
+							tvrageContentSummary = tvrageContentSummary + nextEp;
+						} else {
+							// !last
+							tvrageContentSummary = tvrageContentSummary + lastEp;
+						}
+
+						bot.say(to, tvrageContentSummary);
 					}
 				}
 			});
