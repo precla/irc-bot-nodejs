f762afca3b546eb34aaf27a42a00b49d60b34679
precla <aprcel00@fesb.hr>
2015-05-10 11:48:17 +0200

    Added !isup to check if a site is up

--- bot.js
+++ bot.js
@@ -261,6 +261,36 @@ bot.addListener('message', function(nick, to, text, message) {
 				}
 			});
 		}
+	} else if (args[0] == '!isup') {
+		if (!args[1]) {
+			bot.say(to, 'Missing arguments. Usage example: !isup imdb.com , !isup www.imdb.com ');
+		} else {
+			args.shift();
+			// if there's 'http(s)://' remove it
+			var urlToCheck = args[0].replace(/(http:|https:)\/\//g, '');
+			
+			// some sites like google.com don't get checked properly without 'www.'
+			// so it checks if users has included 'www.' in link, if not add 'www.'
+			if (!urlToCheck.match(/(www\.)/g)) {
+				urlToCheck = 'www.' + urlToCheck;
+			}
+			// before request, check if url is ok, for example: www.example.123 would not allow request
+			if (urlToCheck.match(/(www\.)\w+(\.)[a-zA-Z]/g)) {
+				request('http://www.isup.me/' + urlToCheck, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						if (body.match(/It\'s just you/g)) {
+							bot.say(to, 'Site is up!');
+						} else if (body.match(/Huh\?/g)) { 
+							bot.say(to, 'Sorry pal, that url does not seem to be correct..');
+						} else {
+							bot.say(to, 'Site is down!');
+						}
+					}
+				});
+			} else {
+				bot.say(to, 'Sorry pal, that url does not seem to be correct..');
+			}
+		}
 	} else if (args[0] == '!help') {
 		bot.say(nick, 'Commands available:\n!wp - wikipedia summary\n!weather - current weather\n!tv, !next, !last - for tv show info\!help');
 	}
