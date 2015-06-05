a5b1fbd5cf3632caf2df325dad61b5e0e4f23d15
precla <aprcel00@fesb.hr>
2015-06-06 01:19:39 +0200

    Added config.example.js

---
+++ config.example.js
@@ -0,0 +1,24 @@
+// Create the configuration
+module.exports = {
+	server: 'SERVER',
+	port: 7000,
+	secure: true,
+	selfSigned: true,
+	autoConnect: true,
+	userNick: 'BOTNICK',
+	userName: 'BOTNAME',
+	realName: 'BOTNAME',
+	adminNick: 'ADMIN-NICK',
+	channels: ['#YOURCHAN'],
+	debug: true,
+	showErrors: true,
+	floodProtectionDelay: 1000,
+	messageSplit: 512,
+
+	// NickServ Password:
+	nickservPassword: '',
+	// Youtube Key
+	youtubeKey: '',
+	// Openweather API key:
+	OWapiKey: ''
+};
