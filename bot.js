8de0abe8cacbd08671638884b4798ee5977d63db
precla <precla@users.noreply.github.com>
2019-06-30 11:15:20 +0200

    Add sunrise + sunset for weather output

--- bot.js
+++ bot.js
@@ -193,9 +193,6 @@ bot.addListener('message', function(nick, to, text) {
 						console.error('error while trying to get weather, "cod" code: ', openweatherJson.cod);
 						bot.say(to, 'Sorry, no weather info for that one.');
 					} else if (openweatherJson.cod === 200) {
-						// sunrise & sunset are currently not in use, uncomment if you want to use:
-						// var sunrise = new Date(openweatherJson.sys.sunrise * 1000);
-						// var sunset = new Date(openweatherJson.sys.sunset * 1000);
 
 						var openweatherSummary = 'The current temperature in ' + openweatherJson.name +
 												', ' + openweatherJson.sys.country + ' is: ' + openweatherJson.main.temp.toFixed(1) +
@@ -203,7 +200,9 @@ bot.addListener('message', function(nick, to, text) {
 												'. Pressure: ' + openweatherJson.main.pressure +
 												' hpa. Wind speed: ' + openweatherJson.wind.speed + ' m/s (' + (openweatherJson.wind.speed * 3.6).toFixed(2) +
 												' km/h). Humidity: ' + openweatherJson.main.humidity +
-												' %. Local Time: ' + moment.unix(parseInt(moment().format("X")) + parseInt(openweatherJson.timezone)).format("dddd, MMMM Do YYYY, HH:mm:ss") + '.';
+												' %. Local Time: ' + moment.unix(parseInt(moment().format('X')) + parseInt(openweatherJson.timezone)).format('dddd, MMMM Do YYYY, HH:mm:ss') +
+												' h. Sunrise: ' + moment.unix(openweatherJson.sys.sunrise + parseInt(openweatherJson.timezone)).format('HH:mm:ss') +
+												' h, sunset: ' + moment.unix(openweatherJson.sys.sunset + parseInt(openweatherJson.timezone)).format('HH:mm:ss') + ' h.';
 
 						bot.say(to, openweatherSummary);
 					} else {
